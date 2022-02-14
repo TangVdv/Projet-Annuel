c3bf06d04aeddcb6b7209476b22a9f77bca82dff
Goenae <aozorra.bluesky02@gmail.com>
2022-02-14 20:43:07 +0100

    Very start of the page

---
+++ db_connection.php
@@ -0,0 +1,4 @@
+<?php
+  $db = new PDO('mysql:host=localhost;dbname=pa2', 'root', 'root' ,[PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+
+ ?>
---
+++ panier.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Panier</title>
+
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+  </head>
+  <body>
+    <?php include("db_connection.php");
+      //$req = $db->query('SELECT nom, prix, reduction FROM PRODUIT WHERE (SELECT id_produit FROM AJOUTE) = (SELECT id_produit FROM PRODUIT)');
+    ?>
+    <div class="mx-auto" style="background: #2F91E4; width: 500px; height: 500px; display: block;">
+
+    </div>
+  </body>
+</html>
