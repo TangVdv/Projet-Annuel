8ac63b7a59c1b4fd4d10760431338d1c948f10c8
bingchillin <mohamedelfakharany992@gmail.com>
2022-02-21 15:49:42 +0100

    Add header.php & footer.php
    images & all

---
+++ include/header.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Header</title>
+    <style>
+      header {
+        background : #004579;
+      }
+    </style>
+  </head>
+  <body>
+
+    <header class="py-3 border-bottom">
+      <div class="container d-flex flex-wrap justify-content-center">
+        <a href="/" class="d-flex align-items-center mb-3 mb-lg-0 me-lg-auto text-dark text-decoration-none">
+          <img src="../img/logo_loyaltycard.png" width="40%">
+        </a>
+        <ul class="nav">
+          <li class="nav-item"><a href="#" class="nav-link text-white px-2">Compte</a></li>
+        </ul>
+        <form class="w-100 me-3">
+          <input type="search" class="form-control" placeholder="Vous cherchez un produit ?" aria-label="Search">
+        </form>
+      </div>
+    </header>
+
+    <nav class="py-2 bg-light border-bottom">
+      <div class="container d-flex flex-wrap justify-content-center">
+        <ul class="nav">
+          <li class="nav-item"><a href="#" class="nav-link link-dark px-2 active" aria-current="page">Home</a></li>
+          <li class="nav-item"><a href="#" class="nav-link link-dark px-2">Features</a></li>
+          <li class="nav-item"><a href="#" class="nav-link link-dark px-2">Pricing</a></li>
+          <li class="nav-item"><a href="#" class="nav-link link-dark px-2">FAQs</a></li>
+          <li class="nav-item"><a href="#" class="nav-link link-dark px-2">About</a></li>
+        </ul>
+
+      </div>
+    </nav>
+
+  </body>
+</html>
