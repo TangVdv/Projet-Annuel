34ab09394f864891817cf27d5ca0a78ee0ca7e00
TangVdv <tangrider77120@gmail.com>
2022-02-22 15:32:26 +0100

    Add database connection

--- includes/bdd.php
+++ includes/bdd.php
@@ -1,13 +1,17 @@
 <?php
-function OpenDb()
-{
+function OpenDb(){
    $dbhost = "localhost";
    $dbuser = "root";
    $dbpass = "root";
-   $db = "loyaltycard";
+   $dbname = "loyaltycard";
+   $port = 3306;
 
-   //$db = new PDO('mysql:host='.$dbhost.';dbname='.$db,$dbuser,$dbpass);
-   $db = new PDO('mysql:host=localhost;port=8889;dbname= mysql ','root','root');
+   $db = new PDO('mysql:host='.$dbhost.';port='.$port.';dbname='.$dbname,$dbuser,$dbpass);
    return $db;
+}
+
+function SelectAll($table_name, $db){
+  $req = $db->query('SELECT * FROM '.$table_name);
+  return $req;
 }
 ?>
--- market/index.php
+++ market/index.php
@@ -12,85 +12,30 @@
       </nav>
     </header>
     <main>
-      <div class="d-flex col justify-content-center">
-        <div class="row mb-2">
-          <div class="col text-center m-4">
-            <div class="col g-0 border rounded mb-4 shadow-sm ">
-              <div class="col">
-                <img class="bd-placeholder-img" width="300" height="400" src="../image/test.png" alt="">
-              </div>
-              <div class="col p-4 d-flex flex-column position-static">
-                  <h4 class="mb-0">Name</h4>
-                  <div class="mb-1 text-muted">Description</div>
-                  <strong class="d-inline-block mb-2 mt-2 text-secondary">Price</strong>
+      <div class="row row-cols-md-3 justify-content-center">
+        <?php
+        include("../includes/bdd.php");
+        $db = OpenDb();
+        $res = SelectAll("Produit", $db);
+        while ($row = $res->fetch(PDO::FETCH_OBJ)) {
+        ?>
+            <div class="row mb-2">
+              <div class="col text-center m-4">
+                <div class="col g-0 border rounded mb-4 shadow-sm ">
+                  <div class="col">
+                    <img class="bd-placeholder-img" width="300" height="400" src=<?php echo "../image/products/".$row->image; ?> alt="">
+                  </div>
+                  <div class="col p-4 d-flex flex-column position-static">
+                      <h4 class="mb-0"><?php echo $row->nom; ?></h4>
+                      <div class="mb-1 text-muted">Description</div>
+                      <strong class="d-inline-block mb-2 mt-2 text-secondary"><?php echo $row->prix." €"; ?></strong>
+                    </div>
                 </div>
-            </div>
-          </div>
-          <div class="col text-center m-4">
-            <div class="col g-0 border rounded mb-4 shadow-sm ">
-              <div class="col">
-                <img class="bd-placeholder-img" width="300" height="400" src="../image/test.png" alt="">
               </div>
-              <div class="col p-4 d-flex flex-column position-static">
-                  <h4 class="mb-0">Name</h4>
-                  <div class="mb-1 text-muted">Description</div>
-                  <strong class="d-inline-block mb-2 mt-2 text-secondary">Price</strong>
-                </div>
             </div>
-          </div>
-          <div class="col text-center m-4">
-            <div class="col g-0 border rounded mb-4 shadow-sm ">
-              <div class="col">
-                <img class="bd-placeholder-img" width="300" height="400" src="../image/test.png" alt="">
-              </div>
-              <div class="col p-4 d-flex flex-column position-static">
-                  <h4 class="mb-0">Name</h4>
-                  <div class="mb-1 text-muted">Description</div>
-                  <strong class="d-inline-block mb-2 mt-2 text-secondary">Price</strong>
-                </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="d-flex col justify-content-center">
-        <div class="row mb-2">
-          <div class="col text-center m-4">
-            <div class="col g-0 border rounded mb-4 shadow-sm ">
-              <div class="col">
-                <img class="bd-placeholder-img" width="300" height="400" src="../image/test.png" alt="">
-              </div>
-              <div class="col p-4 d-flex flex-column position-static">
-                  <h4 class="mb-0">Name</h4>
-                  <div class="mb-1 text-muted">Description</div>
-                  <strong class="d-inline-block mb-2 mt-2 text-secondary">Price</strong>
-                </div>
-            </div>
-          </div>
-          <div class="col text-center m-4">
-            <div class="col g-0 border rounded mb-4 shadow-sm ">
-              <div class="col">
-                <img class="bd-placeholder-img" width="300" height="400" src="../image/test.png" alt="">
-              </div>
-              <div class="col p-4 d-flex flex-column position-static">
-                  <h4 class="mb-0">Name</h4>
-                  <div class="mb-1 text-muted">Description</div>
-                  <strong class="d-inline-block mb-2 mt-2 text-secondary">Price</strong>
-                </div>
-            </div>
-          </div>
-          <div class="col text-center m-4">
-            <div class="col g-0 border rounded mb-4 shadow-sm ">
-              <div class="col">
-                <img class="bd-placeholder-img" width="300" height="400" src="../image/test.png" alt="">
-              </div>
-              <div class="col p-4 d-flex flex-column position-static">
-                  <h4 class="mb-0">Name</h4>
-                  <div class="mb-1 text-muted">Description</div>
-                  <strong class="d-inline-block mb-2 mt-2 text-secondary">Price</strong>
-                </div>
-            </div>
-          </div>
-        </div>
+        <?php
+        }
+        ?>
       </div>
     </main>
   </body>
