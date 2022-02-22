6df3dcb3b452b6837e0920246acdf480bc46db1a
Goenae <aozorra.bluesky02@gmail.com>
2022-02-22 17:11:23 +0100

    Fixed sql req for "PRODUIT"

--- panier.php
+++ panier.php
@@ -20,18 +20,33 @@
     ?>
     <div class="mx-auto border border-primary" style="background: #2F91E4; width: 500px; height: 500px; display: block;">
       <?php
-      /*
-        $req = 'SELECT nom, prix, reduction FROM PRODUIT WHERE (SELECT id_produit FROM ACHETE) = (SELECT id_produit FROM PRODUIT)';
+        /*
+        $req = 'SELECT image, nom, prix, reduction
+                  FROM PRODUIT
+                  INNER JOIN ACHETE ON produit.id_produit = achete.id_produit
+                  INNER JOIN UTILISATEUR ON achete.id_utilisateur = 1 ';*/
+
+        $req = 'SELECT image, nom, prix, reduction
+                  FROM PRODUIT
+                  INNER JOIN ACHETE ON produit.id_produit = achete.id_produit
+                  WHERE achete.id_utilisateur = 1 ';
+
         foreach ($db->query($req) as $row) {
-          print $row['nom'] . "\t";
-          print  $row['prix'] . "\t";
-          print $row['reduction'] . "\n";
+          /*
+          echo $row['image'] . "\n";
+          echo $row['nom'] . "\n";
+          echo  $row['prix'] . "\n";
+          echo $row['reduction'] . "\n";*/
+          $image = $row['image'];
+          $nom = $row['nom'];
+          $prix = $row['prix'];
+          $reduction = $row['reduction'];
         }
-        */
+
        ?>
        <div class="d-flex flex-column bd-highlight mb-3">
          <div class="d-flex bd-highlight">
-           <div id="img" class=""></div>
+           <img src="" alt="">
            <div class="w-50 p-2 flex-grow-1 bd-highlight border border-primary">Nom</div>
            <div class="w-25 p-2 bd-highlight border border-primary">11,11€</div>
            <div class="w-15 p-2 bd-highlight border border-primary">41,11%</div>
