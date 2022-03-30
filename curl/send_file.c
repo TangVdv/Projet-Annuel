28a99191f14d762d3e8228890e6327730b5d9851
TangVdv <tangrider77120@gmail.com>
2022-03-30 19:26:07 +0200

    clean code and add local database

--- curl/send_file.c
+++ curl/send_file.c
@@ -3,47 +3,42 @@
 #include <string.h>
 #include <sqlite3.h>
 
-CURL *curl;
 sqlite3 *db;
 sqlite3_stmt *res;
 char *err_msg = 0;
 FILE *yaml_file;
 
+
 int getData(void *NotUsed, int rowCount, char **rowValue, char **rowName){
-  char name[2][50] = {"nom", "prix"};
-	yaml_file = fopen("data.yaml", "a+");
-  for (int i = 0; i < strlen(name[0]); ++i)
-  {
-    fprintf(yaml_file, "%s: %s", name[i], rowValue[i]);
+  for (int i = 0; i < rowCount; i++){
+    fprintf(yaml_file, "%s: %s\n", rowName[i], rowValue[i]);
   }
+
 }
 
-int main(){
-	int rc = sqlite3_open("loyaltycard.db", &db); // Ouvre la base de donnée
-	// Check si la base de donnée existe
-    if (rc != SQLITE_OK) {
-        fprintf(stderr, "Cannot open database: %s\n", sqlite3_errmsg(db));
-        sqlite3_close(db);
-        return 1;
-    }
-    yaml_file = fopen("data.yaml", "w");
+void getSupply(){
+  fprintf(yaml_file, "approvisionnement:\n");
 
+  //sql = "SELECT nom, prix FROM PRODUIT INNER JOIN STOCK ON stock.id_produit = produit.id_produit WHERE CAST(stock.date_ajout AS DATE) = CAST( GETDATE() AS DATE)";
+  //sqlite3_exec(db, sql, getData, 0,&err_msg);
 
-	//VENTE
-	fprintf(yaml_file, "vente:\n");
+  char *sql = "SELECT nom, prenom FROM utilisateur";
+  sqlite3_exec(db, sql, getData, 0,&err_msg);
+}
 
-    char *sql = "SELECT nom, prix FROM PRODUIT INNER JOIN HISTORIQUE_ACHAT ON historique_achat.id_produit  = produit.id_produit WHERE CAST(date_achat AS DATE) = CAST( GETDATE() AS DATE)";
-    sqlite3_exec(db, sql, getData, 0,&err_msg);
+void getSale(){
+  fprintf(yaml_file, "vente:\n");
 
+  //char *sql = "SELECT nom, prix FROM PRODUIT INNER JOIN HISTORIQUE_ACHAT ON historique_achat.id_produit  = produit.id_produit WHERE CAST(date_achat AS DATE) = CAST( GETDATE() AS DATE)";
+  //sqlite3_exec(db, sql, getData, 0,&err_msg);
 
-    //APPROVISIONNEMENT
-    fprintf(yaml_file, "approvisionnement:\n");
+  char *sql = "SELECT nom, description, prix FROM produit";
+  sqlite3_exec(db, sql, getData, 0,&err_msg);
+}
 
-    sql = "SELECT nom, prix FROM PRODUIT INNER JOIN STOCK ON stock.id_produit = produit.id_produit WHERE CAST(stock.date_ajout AS DATE) = CAST( GETDATE() AS DATE)";
-    sqlite3_exec(db, sql, getData, 0,&err_msg);
+void curl(){
+    CURL *curl = curl_easy_init();
 
-
-	curl = curl_easy_init();
     if(curl){
 
         curl_easy_perform(curl);
@@ -51,7 +46,7 @@ int main(){
     }
 
     //upload to this place 
-    curl_easy_setopt(curl, CURLOPT_URL,"./");
+    curl_easy_setopt(curl, CURLOPT_URL,"../../receive_data/");
  
     //tell it to "upload" to the URL 
     curl_easy_setopt(curl, CURLOPT_UPLOAD, 1L);
@@ -63,8 +58,37 @@ int main(){
     curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
  
     curl_easy_perform(curl);
+}
 
-    sqlite3_finalize(res); // Supprime le "statement" de la base de donnée
-    sqlite3_close(db); // Ferme la base de donnée
-    return 0;
+int main(int argc, char * argv[]){
+
+  // OPEN
+  yaml_file = fopen("data.yaml", "w");
+
+	int rc = sqlite3_open("loyaltycard", &db); // Ouvre la base de donnée
+	// Check si la base de donnée existe
+  if (rc != SQLITE_OK) {
+      fprintf(stderr, "Cannot open database: %s\n", sqlite3_errmsg(db));
+      sqlite3_close(db);
+      return 1;
+  }
+
+
+	//VENTE
+	getSale();
+
+
+  //APPROVISIONNEMENT
+  getSupply();
+
+  //CURL
+  curl();
+
+
+  // CLOSE
+  fclose(yaml_file);
+
+  sqlite3_finalize(res); // Supprime le "statement" de la base de donnée
+  sqlite3_close(db); // Ferme la base de donnée
+  return 0;
 }
