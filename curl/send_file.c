c2db53489cb4566e2d0b36cc2ecfa4102c8b0a65
TangVdv <tangrider77120@gmail.com>
2022-04-05 10:34:04 +0200

    add check size of file and if it exist

--- curl/send_file.c
+++ curl/send_file.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <sqlite3.h>
+#include <sys/stat.h>
 
 sqlite3 *db;
 sqlite3_stmt *res;
@@ -42,28 +43,61 @@ void getSale(){
   sqlite3_exec(db, sql, getData, 0,&err_msg);
 }
 
-void curl(){
+int curl(){
     CURL *curl = curl_easy_init();
+    CURLcode res;
+    struct stat file_info;
+    curl_off_t speed_upload, total_time;
+
+
+    if(!yaml_file){
+      printf("error cannot open file");
+      return 1; /* cannot continue */
+    }
+
+    if(fstat(fileno(yaml_file), &file_info) != 0){
+      printf("error file size");
+      return 1; /* cannot continue */
+    }
 
     if(curl){
 
-        curl_easy_perform(curl);
-        curl_easy_cleanup(curl);
-    }
+      //upload to this place 
+      curl_easy_setopt(curl, CURLOPT_URL,"file:///home/tangvdv/Documents/receive_data");
+   
+      //tell it to "upload" to the URL 
+      curl_easy_setopt(curl, CURLOPT_UPLOAD, 1L);
+   
+      //set where to read from (on Windows you need to use READFUNCTION too)
+      curl_easy_setopt(curl, CURLOPT_READDATA, yaml_file);
 
-    //upload to this place 
-    curl_easy_setopt(curl, CURLOPT_URL,"../../receive_data/");
- 
-    //tell it to "upload" to the URL 
-    curl_easy_setopt(curl, CURLOPT_UPLOAD, 1L);
- 
-    //set where to read from (on Windows you need to use READFUNCTION too)
-    curl_easy_setopt(curl, CURLOPT_READDATA, yaml_file);
- 
-    //enable verbose for easier tracing
-    curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
- 
-    curl_easy_perform(curl);
+      /* and give the size of the upload (optional) */
+      curl_easy_setopt(curl, CURLOPT_INFILESIZE_LARGE,
+                     (curl_off_t)file_info.st_size);
+   
+      //enable verbose for easier tracing
+      curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+   
+      res = curl_easy_perform(curl);
+      /* Check for errors */
+      if(res != CURLE_OK) {
+        fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                curl_easy_strerror(res));
+      }
+      else {
+        /* now extract transfer info */
+        curl_easy_getinfo(curl, CURLINFO_SPEED_UPLOAD_T, &speed_upload);
+        curl_easy_getinfo(curl, CURLINFO_TOTAL_TIME_T, &total_time);
+   
+        fprintf(stderr, "Speed: %lu bytes/sec during %lu.%06lu seconds\n",
+                (unsigned long)speed_upload,
+                (unsigned long)(total_time / 1000000),
+                (unsigned long)(total_time % 1000000));
+      }
+      /* always cleanup */
+      curl_easy_cleanup(curl);
+    }
+    return 0;
 }
 
 int main(int argc, char **argv){
