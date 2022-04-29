a1a5fea5870329364b42f9d0284e4aaf386774a2
Goenae <aozorra.bluesky02@gmail.com>
2022-04-29 12:08:55 +0200

    Local payment available

--- panier/PanierModel.php
+++ panier/PanierModel.php
@@ -85,6 +85,7 @@ class PanierModel{
       $prix_total += $row->prix * $row->quantite;
     }
     PanierModel::redeemPoints($UserId, $prix_total);
+
   }
 
   public static function redeemPoints($UserId, $prix_total){
@@ -97,6 +98,71 @@ class PanierModel{
              "prix_total" => $prix_total * 10,
              "id_utilisateur" => $UserId
            ]);
+  }
+
+
+  public static function compareLocalMoney($UserId, $prix_total){
+    include("../includes/bdd.php");
+    //Récupère le solde euro de l'utilisateur
+    $req = $db->prepare('SELECT solde_euro
+                          FROM UTILISATEUR
+                          WHERE id_utilisateur = :id_utilisateur');
+           $req->execute([
+             "id_utilisateur" => $UserId
+           ]);
+    $solde_final = 0;
+    while ($row = $req->fetch(PDO::FETCH_OBJ)){
+      $solde_final = $row->solde_euro;
+    }
+
+    if($prix_total <= $solde_final){
+      return 0;
+    }else {
+      return 1;
+    }
+  }
+
+  public static function getTotalPrice($UserId){
+    //Regroupe la sélection des informations produits et utilisateur ainsi que le remplissage de l'historique d'achat
+    $finalReq = PanierModel::SelectProducts($UserId);
+    $prix_total = 0;
+    while ($row = $finalReq->fetch(PDO::FETCH_OBJ)){
+      $prix_total += $row->prix * $row->quantite;
+    }
+    return $prix_total;
+  }
+
+  public static function updateLocalMoney($prix_total, $UserId){
+    include("../includes/bdd.php");
+
+    //Met à jour le solde euro
+    $req = $db->prepare('UPDATE UTILISATEUR
+                          SET solde_euro = solde_euro - :prix_total
+                          WHERE id_utilisateur = :id_utilisateur');
+           $req->execute([
+             "prix_total" => $prix_total,
+             "id_utilisateur" => $UserId
+           ]);
+  }
+
+  public static function tryWithLocalMoney($UserId){
+    $prix_total = PanierModel::getTotalPrice($UserId);
+    $compareResult = PanierModel::compareLocalMoney($UserId, $prix_total);
+    if($compareResult == 0){
+      PanierModel::UpdateBuyingStatus($UserId);
+      PanierModel::updateLocalMoney($prix_total, $UserId);
+      //Regroupe la sélection des informations produits et utilisateur ainsi que le remplissage de l'historique d'achat
+      $finalReq = PanierModel::SelectProducts($UserId);
+      while ($row = $finalReq->fetch(PDO::FETCH_OBJ)){
+        PanierModel::SaveBuyingHistory($row->prix, $row->quantite, $UserId, $row->id_produit);
+      }
+      //Supprime tous les éléments du panier achetés par l'utilisateur
+      PanierModel::ClearPanier($UserId);
+
+      return 0;
+    }
+    return 1;
+
   }
 
 }
